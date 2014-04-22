82146de1cf752cfa2e78eeb21622ed991cb0eed8
DigitalVerve <info@digitalverve.de>
2014-04-22 05:43:04 +0200

    command line commit

--- provider.js
+++ provider.js
@@ -4,5 +4,5 @@ window.kdnr = "20002";
 // simulate logged in
 // f.e. 042cb1572ffbea5d = 20001 = info@digitalvervede
 // 3a499b457111f803 = welcome@robert-spengler.de / RS97356
-window.aoid = "3a499b457111f803"; 
-window.uid = "3a499b457111f803";
+window.aoid = ""; // 3a499b457111f803
+window.uid = ""; // 3a499b457111f803
